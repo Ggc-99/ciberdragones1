e74cc8353bae961a23e50fb09129008cdeeb18c3
ESTADIA <19307026@utfv.edu.mx>
2023-02-20 17:34:50 -0600

    20-02-2023

---
+++ larespuesta.php
@@ -0,0 +1,17 @@
+<?php
+error_reporting(E_ALL ^ E_DEPRECATED);
+header("Content-Type: text/html; Charset=UTF-8");
+
+$usuario = $_POST['usuario'];
+$password = $_POST['password'];
+
+
+echo $usuario;
+echo"<br>";
+echo $password;
+
+
+
+
+?>
+ 
